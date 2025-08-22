b3fa122d3d9897d56db2e83c9ca1ad85c9c82fdd
PauloS5 <paulocratesdesouza@gmail.com>
2025-08-22 16:50:17 -0500

    feat: ExampleGateway adicionado

---
+++ root/php/examples/table_data_gateway/ExampleGateway.php
@@ -0,0 +1,34 @@
+<?php
+
+/* EXEMPLO DE GATEWAY USADO COM TABLE DATA GATEWAY */
+
+class ExampleGateway {
+    // Atributos
+    private static $conn;
+
+    // Método para estabelecer conexão com o banco
+    public static function setConnection($conn) {
+        self::$conn = $conn;
+    }
+
+    // Métodos de consulta ao banco de dados
+    public function findAll() {
+        /* POR CÓDIGO AQUI */
+    }
+    public function findById($id) {
+        /* POR CÓDIGO AQUI */
+    }
+
+    // Métodos de manipulação do banco de dados
+    public function save($data) {
+        /* POR CÓDIGO AQUI */
+    }
+    public function delete($id) {
+        /* POR CÓDIGO AQUI */
+    }
+
+    // Métodos auxiliares
+    public function getLastid() {
+        /* POR CÓDIGO AQUI */
+    }
+}

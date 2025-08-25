2624a25180d66be9b0bc2d9bfdc42921796bec71
PauloS5 <paulocratesdesouza@gmail.com>
2025-08-25 17:11:29 -0500

    Validators.php criado - Ref. #29

---
+++ root/php/Utils/Validators.php
@@ -0,0 +1,16 @@
+<?php
+
+class Validators {
+    public static function validateEmail() {
+        ;
+    }
+    public static function validateCpf() {
+        ;
+    }
+    public static function validatePhoneNumber() {
+        ;
+    }
+    public static function validateCnpj() {
+        ;
+    }
+}

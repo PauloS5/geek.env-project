d8fa0235c4b796e9405ccec536135dd1299e9939
PauloS5 <paulocratesdesouza@gmail.com>
2025-08-28 14:12:16 -0500

    index apagado

--- root/php/Utils/Validators.php
+++ root/php/Utils/Validators.php
@@ -1,16 +1,18 @@
 <?php
 
 class Validators {
-    public static function validateEmail() {
-        ;
+    public static function validateEmail($email) {
     }
-    public static function validateCpf() {
-        ;
+
+    public static function validateCpf($cpf) {
     }
-    public static function validatePhoneNumber() {
-        ;
+
+    public static function validatePhoneNumber($phone) {
+         $pattern = "/^\([\d]{2}\)[ ]?[\d]{4,5}-[\d]{4}$/";
+
+        return preg_match($pattern, $phone);
     }
-    public static function validateCnpj() {
-        ;
+
+    public static function validateCnpj($cnpj) {
     }
 }
